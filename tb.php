49e8409a4ebb19bb0728a448944c1b85c8ae1a04
Afrede bemya <bemyaalfrede@gmail.com>
2025-04-13 19:26:14 +0200

    main

---
+++ tb.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AviGest - Gestion de Produits Avicoles</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+</head>
+<body>
+ <!-- inclusion de l'entête du site -->
+    <?php require_once(__DIR__ . '/hearder.php'); ?>
+
+
+    <!-- Formulaire de connexion -->
+    <?php require_once(__DIR__ . '/index.php'); ?>
+
+<!-- inclusion du bas de page du site -->
+<?php require_once(__DIR__ . '/footer.php'); ?>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+ <script src="app.js"></script>
+</body>
+</html>
